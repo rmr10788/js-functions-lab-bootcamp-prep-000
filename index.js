03d5c522f71761bd5421dd68e72ed4ca9d42690b
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-02 00:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code below!
+var name = 'you'
+
 function happyHolidays() {
   return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!'
-  var name = 'you'
 }
 
 function happyHolidayTo(holiday, name) {
